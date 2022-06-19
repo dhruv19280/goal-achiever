2387e56596ae3dcb0ceb7b338ff64163f1f3d454
Dhruv Mathur <dhruv19280@gmail.com>
2022-06-20 00:58:47 +0530

    Added Investment Class

---
+++ src/main/java/com/example/goalachiever/Investment.java
@@ -0,0 +1,25 @@
+package com.example.goalachiever;
+
+import java.util.Date;
+
+public class Investment {
+
+    public String investmentDescription;
+    public GoalRealizationModelType investmentModelType;
+
+    public Date investmentPurchaseDate;
+    public Date investmentSaleDate;
+
+    public Beneficiary investmentOwner1;
+    public Beneficiary investmentOwner2;
+    public Beneficiary investmentOwner3;
+    public String investmentHouse;
+    public String investmentProduct;
+    public String investmentFolio;
+
+    public Number investmentPurchaseAmount;
+    public Number investmentSaleAmount;
+    
+    public Date investmentValueAsOn;
+    public Number investmentValueAmount;
+}
